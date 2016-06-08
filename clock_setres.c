2ea1210e0ed2ba72ed95102c480c74d8aea75d2c
John Hood <cgull@glup.org>
2016-06-07 20:19:36 -0400

    clock_setres: fix argv bug, catch errors, build on non-cygwin

--- clock_setres.c
+++ clock_setres.c
@@ -1,21 +1,32 @@
 #include <errno.h>
-#include <error.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#ifdef __CYGWIN__
 #include <cygwin/time.h>
+#endif
 #include <sys/time.h>
 
 int
 main(int argc, char **argv)
 {
-
+  if (argc < 2) {
+    fprintf(stderr, "no args\n");
+    exit(1);
+  }
   struct timespec ts;
+  char *ep;
   ts.tv_sec = 0;
-  ts.tv_nsec = atoi(argv[1]);
+  ts.tv_nsec = strtol(argv[1], &ep, 0);
+  if (*argv[1] != '\0' && *ep != '\0') {
+    fprintf(stderr, "bad resolution value\n");
+    exit(1);
+  }
+#ifdef __CYGWIN
   clock_setres(CLOCK_REALTIME, &ts);
+#endif
   if (argc > 2) {
-    execvp(argv[2], &argv[3]);
+    execvp(argv[2], argv+2);
     return errno;
   } else {
     pause();
